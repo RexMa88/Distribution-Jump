02d9e77f0d536a724446acb5f1c394da47d6bf06
RexMa88 <rex.smith98@gmail.com>
2016-01-04 21:00:27 +0800

    add AsyncQueue

--- pushOrPop/RMAsyncQueue.h
+++ pushOrPop/RMAsyncQueue.h
@@ -8,8 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
+//创建关联队列
 void RMQueue(dispatch_queue_t q);
 
-//create a custom queue
+//创建一个自定义队列
 dispatch_queue_t RMCreateQueue(const char *label,
                                dispatch_queue_attr_t attr);
+
+//判断队列是否是当前队列
+BOOL RMQueueCurrentIsTaggedQueue(dispatch_queue_t q);
+
+//获取关联队列
+dispatch_queue_t RMQueueGetCurrentTagged();
+
+//判断当前线程是否为主线程
+BOOL RMQueueCurrentIsMainQueue();
+
+//安全的同步线程 防止主线程同步造成死锁
+void RMQueueSafeDispatchSync(dispatch_queue_t q, dispatch_block_t block);
+
+//断言专用 哦呵呵^_^
+#define RMAssertQueue(q)        NSAssert(RMQueueCurrentIsTaggedQueue(q), @"Must run on queue: " #q)
+
+#define RMAssertMainQueue()     NSAssert(RMQueueCurrentIsMainQueue(), @"Must run on main queue")
--- pushOrPop/RMAsyncQueue.m
+++ pushOrPop/RMAsyncQueue.m
@@ -10,13 +10,45 @@
 
 static const char queueTagKey;
 
+/**
+ *  利用队列关联数据的方式存储队列
+ *
+ *  @param q dispatch_queue_t 存储的队列
+ */
 void RMQueue(dispatch_queue_t q){
     dispatch_queue_set_specific(q, &queueTagKey, (__bridge void*)q, NULL);
 }
 
+
+
+/**
+ *  比较便捷地创造一个自定义队列
+ *
+ *  @param label 自定义队列的标记 flag
+ *  @param attr  自定义队列的属性（并行、串行）DISPATCH_QUEUE_SERIAL || DISPATCH_QUEUE_CONCURRENT
+ *
+ *  @return 自定义队列 custom dispatch_queue_t
+ */
 dispatch_queue_t RMCreateQueue(const char *label,
                                dispatch_queue_attr_t attr){
     dispatch_queue_t q = dispatch_queue_create(label, attr);
     RMQueue(q);
     return q;
+}
+
+BOOL RMQueueCurrentIsTaggedQueue(dispatch_queue_t q){
+    return (RMQueueGetCurrentTagged() == q);
+}
+
+dispatch_queue_t RMQueueGetCurrentTagged(){
+    return (__bridge dispatch_queue_t)dispatch_get_specific(&queueTagKey);
+}
+
+BOOL RMQueueCurrentIsMainQueue(){
+    return [NSThread isMainThread];
+}
+
+void RMQueueSafeDispatchSync(dispatch_queue_t q, dispatch_block_t block){
+    if (RMQueueCurrentIsTaggedQueue(q)) block();
+    else dispatch_sync(q, block);
 }
