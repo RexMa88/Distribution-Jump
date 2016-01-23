28050245fa5bd5e07086c4b3ebfb94c41d46b66b
RexMa88 <rex.smith98@gmail.com>
2016-01-23 17:37:18 +0800

    add POICell

---
+++ pushOrPop/RMPOITableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  RMPOITableViewCell.h
+//  pushOrPop
+//
+//  Created by Rex Ma on 16/1/23.
+//  Copyright © 2016年 RexMa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMPOITableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *telLbael;
+
+@end
---
+++ pushOrPop/RMPOITableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RMPOITableViewCell.m
+//  pushOrPop
+//
+//  Created by Rex Ma on 16/1/23.
+//  Copyright © 2016年 RexMa. All rights reserved.
+//
+
+#import "RMPOITableViewCell.h"
+
+@implementation RMPOITableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
